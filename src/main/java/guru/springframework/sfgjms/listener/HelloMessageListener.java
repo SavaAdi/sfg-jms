d193e84b58028d7f020bf467336b5895cfbdcd31
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-10-12 22:05:23 +0300

    Added send and receive jms message

--- src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -2,16 +2,24 @@ package guru.springframework.sfgjms.listener;
 
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
@@ -19,6 +27,19 @@ public class HelloMessageListener {
 
         System.out.println("I got a message");
         System.out.println(helloWorldMessage);
+
+    };
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHEllo(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message) throws JMSException {
+
+        HelloWorldMessage payloadMsg = HelloWorldMessage.builder()
+                .uuid(UUID.randomUUID())
+                .message("Hello")
+                .build();
+
+       jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
 
     };
 
